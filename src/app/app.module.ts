e9b83e49b20208250b6436d4120bd772c0dcc8a9
teo.mendonca1510@gmail.com <teo.mendonca1510@gmail.com>
2024-04-09 21:25:45 -0300

    do not have routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MenuSuperiorComponent } from './menu-superior/menu-superior.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ListaClienteComponent } from './lista-cliente/lista-cliente.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuSuperiorComponent,
+    CarouselComponent,
+    ListaClienteComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
