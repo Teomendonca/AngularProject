cd00d87c9bc85a53fee2df593fb352965a25fc62
teo.mendonca1510@gmail.com <teo.mendonca1510@gmail.com>
2024-05-03 20:08:53 -0300

    feat(edit)/Edit users layout

--- src/app/home/pages/users/edit-users/edit-users.component.ts
+++ src/app/home/pages/users/edit-users/edit-users.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Func } from '../users.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-users',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrl: './edit-users.component.css'
 })
 export class EditUsersComponent {
+  addUserForm: FormGroup;
+  func = Func
+  keys: any[];
+  constructor(private router: Router) {
 
+    this.keys = Object.keys(this.func).filter(Number);
+
+    this.addUserForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('',
+        [Validators.required,
+        Validators.email]),
+      func: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20)]),
+      passwordConfirmation: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20)
+      ])
+    })
+  }
+
+  onSubmit(){
+    console.log("Usuário criado com sucesso")
+    console.log(this.addUserForm.value);
+    this.router.navigate(['app/users'])
+  }
+  cancel(){
+    this.router.navigate(['app/users'])
+  }
 }
